f0de9e230d607225a296db94f335b8e683e1b833
Gonzalo Diamend <gonzalo.diamend@gmail.com>
2023-11-07 15:10:38 -0300

    Adding new env

--- cypress/e2e/apiTest.cy.js
+++ cypress/e2e/apiTest.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Caramel API - Admin tool ', () => {
+  it('Caramel - Valid Log in', () => {
+    cy.api({
+      method: 'GET',
+      url: '/users/login',
+      failOnStatusCode: false,
+      body: {
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+
+      });
+    });
+  });
