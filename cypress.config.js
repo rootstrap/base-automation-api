47793f9163ac3f568d28818a89742820809b59c4
Gonzalo Diamend <gonzalo.diamend@gmail.com>
2023-11-07 11:51:20 -0300

    Adding initial setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,39 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  
   e2e: {
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    defaultCommandTimeout: 15000,
+    chromeWebSecurity: false,
+    video: false,
+    snapshotOnly: true,
+    experimentalMemoryManagement: true,
+    experimentalModifyObstructiveThirdPartyCode: true,
+
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+      })
+      const environmentName = config.env.environmentName || 'qa';
+      const environmentFilename = `./${environmentName}.settings.json`;
+      console.log('loading %s', environmentFilename);
+      const settings = require(environmentFilename);
+      if (settings.baseUrl) {
+        config.baseUrl = settings.baseUrl;
+      }
+      if (settings.env) {
+        config.env = {
+          ...config.env,
+          ...settings.env,
+        };
+      }
+      console.log('loaded settings for environment %s', environmentName);
+
+
+      return config;
+
     },
   },
 });
